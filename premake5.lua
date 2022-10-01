0fdbf1d7ad788cff9705b4699f293f7342a57227
Thomas Lessard <thomaslessard122@hotmail.com>
2022-10-01 17:25:39 -0400

    Update premake

--- premake5.lua
+++ premake5.lua
@@ -46,7 +46,13 @@ project "ImGui"
 		runtime "Release"
 		symbols "Off"
 
-    filter { ("configurations:Release" or "configurations:Dist") and "system:windows" }
+    filter { "configurations:Release", "system:windows" }
+		flags
+		{
+			"LinkTimeOptimization"
+		}
+
+	filter { "configurations:Dist", "system:windows" }
 		flags
 		{
 			"LinkTimeOptimization"
